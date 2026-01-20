bf8a05e9d723fe16ea66a351f76d6d52f58f18cc
Hoa Nguyen <hoanguyensm.cc@gmail.com>
2026-01-20 10:29:59 +0700

    fix null value for price change

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -14,10 +14,10 @@ const CoinCard = ({ coin }) => {
         <p>Price: ${coin.current_price.toLocaleString()}</p>
         <p
           className={
-            coin.price_change_percentage_24h >= 0 ? "positive" : "negative"
+            (coin.price_change_percentage_24h ?? 0) >= 0 ? "positive" : "negative"
           }
         >
-          {coin.price_change_percentage_24h.toFixed(2)} %
+          {coin.price_change_percentage_24h?.toFixed(2) ?? "N/A"} %
         </p>
         <p>Maret cap: {coin.market_cap.toLocaleString()}</p>
       </div>
